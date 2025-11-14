c10c322e98fb5cadc7f1f5e48acd47613fb68dc2
Artmaster629 <ponedilokartem2@gmail.com>
2025-11-14 22:13:10 +0200

    Удалены команды из openpacbp, добавлена команда на открытие GUI прокачки

--- src/main/java/net/artmaster/era_tweaks/command/ModCommands.java
+++ src/main/java/net/artmaster/era_tweaks/command/ModCommands.java
@@ -4,17 +4,11 @@ import net.artmaster.era_tweaks.ModMain;
 import net.artmaster.era_tweaks.network.Network;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
-import net.minecraft.network.chat.Component;
-import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerPlayer;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.event.RegisterCommandsEvent;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.neoforge.server.ServerLifecycleHooks;
-import xaero.pac.common.server.api.OpenPACServerAPI;
-import xaero.pac.common.server.parties.party.api.IPartyManagerAPI;
-import xaero.pac.common.server.parties.party.api.IServerPartyAPI;
 
 
 @Mod("era_tweaks")
@@ -24,22 +18,11 @@ public class ModCommands {
     @SubscribeEvent
     public static void registerCommands(RegisterCommandsEvent event) {
         event.getDispatcher().register(
-                Commands.literal("guild")
+                Commands.literal("upgrade")
                         .executes(ctx -> {
                             CommandSourceStack source = ctx.getSource();
                             if (source.getEntity() instanceof ServerPlayer player) {
-                                MinecraftServer server = ServerLifecycleHooks.getCurrentServer();
-
-                                if (server == null) return 0;
-
-                                IPartyManagerAPI partyManager = OpenPACServerAPI.get(server).getPartyManager();
-                                IServerPartyAPI party = partyManager.getPartyByMember(player.getUUID());
-                                if (party == null) {
-                                    Network.sendAllyParties(player, party);
-                                    player.displayClientMessage(Component.translatable("text.era_tweaks.no_party_pm"), false);
-                                    return 0;
-                                }
-                                Network.sendOpenGui(player);
+                                Network.sendOpenGui(player, "upgrade_manage_screen");
                             }
                             return 1;
                         })
