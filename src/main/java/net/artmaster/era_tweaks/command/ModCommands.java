6fab74e1166878a13b26ac3c17c9255ae8ec5e23
Artmaster629 <ponedilokartem2@gmail.com>
2025-12-07 17:09:48 +0200

    Добавлены команды на установку названия класса и открытие других экранов

--- src/main/java/net/artmaster/era_tweaks/command/ModCommands.java
+++ src/main/java/net/artmaster/era_tweaks/command/ModCommands.java
@@ -1,9 +1,13 @@
 package net.artmaster.era_tweaks.command;
 
+import com.mojang.brigadier.arguments.StringArgumentType;
 import net.artmaster.era_tweaks.ModMain;
+import net.artmaster.era_tweaks.api.container.MyAttachments;
+import net.artmaster.era_tweaks.api.container.PlayerClassData;
 import net.artmaster.era_tweaks.network.Network;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
@@ -22,11 +26,52 @@ public class ModCommands {
                         .executes(ctx -> {
                             CommandSourceStack source = ctx.getSource();
                             if (source.getEntity() instanceof ServerPlayer player) {
+                                Network.syncSkills(player);
+                                Network.syncClasses(player);
                                 Network.sendOpenGui(player, "upgrade_manage_screen");
                             }
                             return 1;
                         })
 
+        );
+        event.getDispatcher().register(
+                Commands.literal("player_class")
+                        .executes(ctx -> {
+                            CommandSourceStack source = ctx.getSource();
+                            if (source.getEntity() instanceof ServerPlayer player) {
+                                Network.syncSkills(player);
+                                Network.syncClasses(player);
+                                Network.sendOpenGui(player, "class_manage_screen");
+                            }
+                            return 1;
+                        })
+
+        );
+
+        event.getDispatcher().register(
+                Commands.literal("player_class")
+                        .then(Commands.literal("set")
+                                .then(Commands.argument("classname", StringArgumentType.string())
+                                        .executes(ctx -> {
+                                            CommandSourceStack source = ctx.getSource();
+                                            String className = StringArgumentType.getString(ctx, "classname");
+
+                                            if (source.getEntity() instanceof ServerPlayer player) {
+
+                                                PlayerClassData data = player.getData(MyAttachments.PLAYER_CLASS);
+                                                data.changePlayerClass(className);
+
+
+                                                Network.syncSkills(player);
+                                                Network.syncClasses(player);
+
+                                                source.sendSuccess(() -> Component.literal("Класс игрока "+player.getName()+" успешно изменён на "+className+"."), true);
+                                            }
+
+                                            return 1;
+                                        })
+                                )
+                        )
         );
     }
 }
