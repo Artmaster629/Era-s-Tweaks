72db59c8ee2e06725e83d1711d7bc738b229da9e
Artmaster629 <ponedilokartem2@gmail.com>
2025-12-21 12:00:21 +0200

    Добавлена система боссбара, в котором отображается прогресс прокачки игрока

---
+++ src/main/java/net/artmaster/era_tweaks/client/BossbarManager.java
@@ -0,0 +1,48 @@
+package net.artmaster.era_tweaks.client;
+
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.BossEvent.BossBarColor;
+import net.minecraft.world.BossEvent.BossBarOverlay;
+import net.minecraft.server.level.ServerBossEvent;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BossbarManager {
+    private static final Map<ServerPlayer, ServerBossEvent> bossbars = new HashMap<>();
+
+    public static void updateBossbar(ServerPlayer player, double progress, double progress_addition, String attribute) {
+
+        Component titleAttribute = Component.translatable("sattribute.era_tweaks."+attribute);
+
+        ServerBossEvent bossbar = bossbars.computeIfAbsent(player, p -> {
+            ServerBossEvent newBar = new ServerBossEvent(
+                    titleAttribute,
+                    BossBarColor.BLUE,
+                    BossBarOverlay.PROGRESS
+            );
+            newBar.setCreateWorldFog(false);
+            newBar.setVisible(true);
+            newBar.addPlayer(player);
+            return newBar;
+        });
+
+
+        double visualProgress = progress;
+        if (progress > 100) {
+            visualProgress=100;
+        }
+        bossbar.setProgress((float) (visualProgress/100)); // от 0.0 до 1.0
+
+
+        bossbar.setName(Component.literal(titleAttribute.getString()+" +"+(double) Math.round(progress_addition*100)/100+" ("+ (int) visualProgress + "%)"));
+    }
+
+    public static void removeBossbar(ServerPlayer player) {
+        ServerBossEvent bar = bossbars.remove(player);
+        if (bar != null) {
+            bar.removePlayer(player);
+        }
+    }
+}
