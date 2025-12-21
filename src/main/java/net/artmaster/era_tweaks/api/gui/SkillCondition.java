f19973ac42279c5e461ae53ca12dbd8ce99eb4f4
Artmaster629 <ponedilokartem2@gmail.com>
2025-12-21 12:25:03 +0200

    Добавлен класс, в котором хранится список строк и условие

---
+++ src/main/java/net/artmaster/era_tweaks/api/gui/SkillCondition.java
@@ -0,0 +1,22 @@
+package net.artmaster.era_tweaks.api.gui;
+
+import java.util.List;
+
+public class SkillCondition {
+
+    private final boolean isRequiredAll;
+    private final List<String> skills;
+
+    public SkillCondition(List<String> skills, boolean isRequiredAll) {
+        this.skills = skills;
+        this.isRequiredAll = isRequiredAll;
+    }
+
+    public boolean isRequiredAll() {
+        return isRequiredAll;
+    }
+
+    public List<String> skills() {
+        return skills;
+    }
+}
