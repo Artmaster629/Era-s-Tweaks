487aff1cddd6f87bc0e6a9b56419ee0568c36ecf
Artmaster629 <ponedilokartem2@gmail.com>
2025-12-21 12:01:30 +0200

    Добавлены методы синхронизации данных прокачки, добавлен метод на потребление стамины

--- src/main/java/net/artmaster/era_tweaks/client/ClientPacketHandler.java
+++ src/main/java/net/artmaster/era_tweaks/client/ClientPacketHandler.java
@@ -1,13 +1,18 @@
 package net.artmaster.era_tweaks.client;
 
+import com.alrex.parcool.api.Stamina;
+import net.artmaster.era_tweaks.api.container.MyAttachments;
+import net.artmaster.era_tweaks.api.container.PlayerClassData;
+import net.artmaster.era_tweaks.api.container.PlayerSAttrubitesData;
 import net.artmaster.era_tweaks.client.screen.ClassManageScreen;
+import net.artmaster.era_tweaks.client.screen.ClassSelectScreen;
 import net.artmaster.era_tweaks.client.screen.UpgradeManageScreen;
-import net.artmaster.era_tweaks.network.OpenGuiPacket;
-import net.artmaster.era_tweaks.network.RunCommandPacket;
+import net.artmaster.era_tweaks.network.*;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.api.distmarker.OnlyIn;
+
 
 @OnlyIn(Dist.CLIENT)
 public class ClientPacketHandler {
@@ -18,8 +23,36 @@ public class ClientPacketHandler {
         if (packet.resourceId().equals("class_manage_screen")) {
             Minecraft.getInstance().setScreen(new ClassManageScreen());
         }
+        if (packet.resourceId().equals("class_select_screen")) {
+            Minecraft.getInstance().setScreen(new ClassSelectScreen());
+        }
     }
 
+
+
+    public static void handleSyncPlayerSkills(SyncPlayerSkillsPacket packet) {
+        var player = Minecraft.getInstance().player;
+        if (player == null) return;
+
+        var data = player.getData(MyAttachments.PLAYER_SKILLS);
+        PlayerSAttrubitesData.load(data, packet.data());
+    }
+
+    public static void handleSyncPlayerClass(SyncPlayerClassPacket packet) {
+        var player = Minecraft.getInstance().player;
+        if (player == null) return;
+        var data = player.getData(MyAttachments.PLAYER_CLASS);
+        PlayerClassData.load(data, packet.data());
+    }
+
+    public static void handleStaminaConsume(StaminaPacket packet) {
+        var player = Minecraft.getInstance().player;
+        if (player == null) return;
+
+
+        var stamina = Stamina.get(player);
+        stamina.consume(packet.stamina());
+    }
 
 
     public static void handleRunCommand(RunCommandPacket packet) {
