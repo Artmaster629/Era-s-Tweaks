0be3143d27869b5bb60ad4156322abcb460f616f
Artmaster629 <ponedilokartem2@gmail.com>
2026-02-02 18:01:25 +0200

    Изменён рендер под новые атрибуты и конфиги

--- src/main/java/net/artmaster/era_tweaks/client/TooltipRender.java
+++ src/main/java/net/artmaster/era_tweaks/client/TooltipRender.java
@@ -1,12 +1,14 @@
 package net.artmaster.era_tweaks.client;
 
 import net.artmaster.era_tweaks.ModMain;
-import net.artmaster.era_tweaks.api.container.MyAttachments;
-import net.artmaster.era_tweaks.api.container.PlayerSAttrubitesData;
+import net.artmaster.era_tweaks.registry.ModAttachments;
+import net.artmaster.era_tweaks.custom.data.PlayerSAttrubitesData;
+import net.artmaster.era_tweaks.config.DeniedByClassConfig;
 import net.artmaster.era_tweaks.config.DeniedConfig;
 import net.minecraft.ChatFormatting;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.resources.ResourceLocation;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
@@ -28,54 +30,100 @@ public class TooltipRender {
         ResourceLocation itemId = BuiltInRegistries.ITEM.getKey(item);
         if (itemId == null) return;
         DeniedConfig.CancelRule rule = DeniedConfig.getList().get(itemId.toString());
-        if (rule == null) return; // нет запрета
+        DeniedByClassConfig.CancelByClassRule classRule = DeniedByClassConfig.getList().get(itemId.toString());
+        if (rule != null && classRule != null) {
+            event.getToolTip().add(Component.literal(""));
+            event.getToolTip().add(Component.literal("§7Требования:"));
+            if (rule != null) {
+                PlayerSAttrubitesData data = player.getData(ModAttachments.PLAYER_SKILLS);
+                Map<String, Integer> playerLevels = Map.of(
+                        "intellect", data.getIntellectLevel(),
+                        "body", data.getBodyLevel(),
+                        "society", data.getSocietyLevel()
+                );
 
-        PlayerSAttrubitesData data = player.getData(MyAttachments.PLAYER_SKILLS);
-        Map<String, Integer> playerLevels = Map.of(
-                "fight", data.getFightLevel(),
-                "magic", data.getMagicLevel(),
-                "crafting", data.getCraftingLevel(),
-                "building", data.getBuildingLevel(),
-                "mining", data.getMiningLevel(),
-                "stamina", data.getStaminaLevel(),
-                "farming", data.getFarmingLevel()
-        );
+                List<String> attrs = rule.requiredAttributes();
+                List<Integer> levels = rule.requiredLevels();
 
-        List<String> attrs = rule.requiredAttributes();
-        List<Integer> levels = rule.requiredLevels();
-
-        //boolean denied = false;
-        event.getToolTip().add(Component.literal(""));
-        event.getToolTip().add(Component.literal("§7Требования:"));
+                //boolean denied = false;
 
 
-        for (int i = 0; i < attrs.size(); i++) {
-            String attr = attrs.get(i);
-            int requiredLevel = levels.get(i);
-            int playerLevel = playerLevels.getOrDefault(attr, 0);
-            if (playerLevel < requiredLevel) {
-                //denied = true;
-                event.getToolTip().add(Component.literal(Component.translatable("sattribute.era_tweaks."+attr).getString()+": "+requiredLevel)
-                        .withStyle(ChatFormatting.RED));
+                for (int i = 0; i < attrs.size(); i++) {
+                    String attr = attrs.get(i);
+                    int requiredLevel = levels.get(i);
+                    int playerLevel = playerLevels.getOrDefault(attr, 0);
+                    if (playerLevel < requiredLevel) {
+                        //denied = true;
+                        event.getToolTip().add(Component.literal("• " + Component.translatable("sattribute.era_tweaks." + attr).getString() + ": " + requiredLevel)
+                                .withStyle(ChatFormatting.RED));
 
-            } else {
-                event.getToolTip().add(Component.literal(Component.translatable("sattribute.era_tweaks."+attr).getString()+": "+requiredLevel)
-                        .withStyle(ChatFormatting.GREEN));
+                    } else {
+                        event.getToolTip().add(Component.literal("• " + Component.translatable("sattribute.era_tweaks." + attr).getString() + ": " + requiredLevel)
+                                .withStyle(ChatFormatting.GREEN));
+                    }
+                }
+
+                String requiredSkill = rule.requiredSkill();
+                if (!requiredSkill.isEmpty()) {
+                    var classData = player.getData(ModAttachments.PLAYER_CLASS);
+                    if (!classData.getPlayerSkills().contains(requiredSkill)) {
+                        event.getToolTip().add(Component.literal("• " + Component.translatable("text.era_tweaks.skill").getString() + ": " + Component.translatable("skill.era_tweaks." + requiredSkill).getString())
+                                .withStyle(ChatFormatting.RED));
+
+                    } else {
+                        event.getToolTip().add(Component.literal("• " + Component.translatable("text.era_tweaks.skill").getString() + ": " + Component.translatable("skill.era_tweaks." + requiredSkill).getString())
+                                .withStyle(ChatFormatting.GREEN));
+                    }
+                }
+
+            }
+            if (classRule != null) {
+                var classData = player.getData(ModAttachments.PLAYER_CLASS);
+
+
+
+                String classStr = classRule.classStr();
+                if (!classStr.isEmpty()) {
+                    if (!classData.getPlayerClass().equals(classStr)) {
+                        event.getToolTip().add(Component.literal("• " + Component.translatable("text.era_tweaks.class").getString() + ": " + Component.translatable("text.era_tweaks." + classStr + "_class").getString())
+                                .withStyle(ChatFormatting.RED));
+
+                    } else {
+                        event.getToolTip().add(Component.literal("• " + Component.translatable("text.era_tweaks.class").getString() + ": " + Component.translatable("text.era_tweaks." + classStr + "_class").getString())
+                                .withStyle(ChatFormatting.GREEN));
+                    }
+                }
+                String subclassStr = classRule.subclassStr();
+                if (!subclassStr.isEmpty()) {
+                    if (!classData.getPlayerSubClass().equals(subclassStr)) {
+                        event.getToolTip().add(Component.literal("• " + Component.translatable("text.era_tweaks.subclass").getString() + ": " + Component.translatable("text.era_tweaks." + subclassStr + "_subclass").getString())
+                                .withStyle(ChatFormatting.RED));
+
+                    } else {
+                        event.getToolTip().add(Component.literal("• " + Component.translatable("text.era_tweaks.subclass").getString() + ": " + Component.translatable("text.era_tweaks." + subclassStr + "_subclass").getString())
+                                .withStyle(ChatFormatting.GREEN));
+                    }
+                }
+
+                List<String> requiredSkills = classRule.requiredSkills();
+                if (!requiredSkills.isEmpty()) {
+
+                    MutableComponent base = Component.literal("§7"+Component.translatable("text.era_tweaks.skills").getString() + ":");
+                    ChatFormatting color = ChatFormatting.RED;
+
+                    for (String skill1 : requiredSkills) {
+                        base.getSiblings().add(Component.literal("\n• " + Component.translatable("skill.era_tweaks." + skill1).getString()));
+                    }
+
+                    for (String skill : requiredSkills) {
+                        if (classData.getPlayerSkills().contains(skill)) {
+                            color = ChatFormatting.GREEN;
+                            break;
+                        }
+                    }
+                    event.getToolTip().add(base.withStyle(color));
+                }
             }
         }
-
-        String requiredSkill = rule.requiredSkill();
-        if (requiredSkill.isEmpty()) return;
-        var classData = player.getData(MyAttachments.PLAYER_CLASS);
-        if (classData.getPlayerSkills().contains(requiredSkill)) {
-            event.getToolTip().add(Component.literal(Component.translatable("text.era_tweaks.skill").getString()+":"+Component.translatable("sattribute.era_tweaks."+requiredSkill).getString())
-                    .withStyle(ChatFormatting.RED));
-
-        } else {
-            event.getToolTip().add(Component.literal(Component.translatable("text.era_tweaks.skill").getString()+":"+Component.translatable("sattribute.era_tweaks."+requiredSkill).getString())
-                    .withStyle(ChatFormatting.GREEN));
-        }
-
-
     }
 }
