6a55410f0c4b8a3ea0dd43cbcdc33c89c41b0137
Artmaster629 <ponedilokartem2@gmail.com>
2026-02-02 17:33:41 +0200

    Удалены старые пакеты синхронизации данных, теперь данные о хранилищах с сервера на клиент отсылаются одним пакетом

--- src/main/java/net/artmaster/era_tweaks/network/main_packets/DataPacket.java
+++ src/main/java/net/artmaster/era_tweaks/network/main_packets/DataPacket.java
@@ -1,4 +1,4 @@
-package net.artmaster.era_tweaks.network;
+package net.artmaster.era_tweaks.network.main_packets;
 
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.FriendlyByteBuf;
@@ -6,15 +6,15 @@ import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.resources.ResourceLocation;
 
-public record SyncPlayerClassPacket(CompoundTag data) implements CustomPacketPayload {
+public record DataPacket(CompoundTag data, int dataType) implements CustomPacketPayload {
 
-    public static final Type<SyncPlayerClassPacket> TYPE =
+    public static final Type<DataPacket> TYPE =
             new Type<>(ResourceLocation.fromNamespaceAndPath("era_tweaks", "sync_player_class"));
 
-    public static final StreamCodec<FriendlyByteBuf, SyncPlayerClassPacket> CODEC =
+    public static final StreamCodec<FriendlyByteBuf, DataPacket> CODEC =
             StreamCodec.of(
-                    (buf, pkt) -> buf.writeNbt(pkt.data),
-                    buf -> new SyncPlayerClassPacket(buf.readNbt())
+                    (buf, pkt) -> {buf.writeNbt(pkt.data); buf.writeInt(pkt.dataType);},
+                    buf -> new DataPacket(buf.readNbt(), buf.readInt())
             );
 
     @Override
