35ae88b5ad604e2988f6eb5440bc535b9b2062a9
Artmaster629 <ponedilokartem2@gmail.com>
2026-02-02 17:44:03 +0200

    Новые кнопки - кнопки переключения активных способностей

--- src/main/java/net/artmaster/era_tweaks/client/ModKeybinds.java
+++ src/main/java/net/artmaster/era_tweaks/client/ModKeybinds.java
@@ -2,10 +2,9 @@ package net.artmaster.era_tweaks.client;
 
 import com.mojang.blaze3d.platform.InputConstants;
 import net.artmaster.era_tweaks.ModMain;
-import net.artmaster.era_tweaks.api.container.MyAttachments;
 import net.artmaster.era_tweaks.client.screen.ClassManageScreen;
-import net.artmaster.era_tweaks.client.screen.ClassSelectScreen;
 import net.artmaster.era_tweaks.client.screen.UpgradeManageScreen;
+import net.artmaster.era_tweaks.network.Network;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
 import net.neoforged.api.distmarker.Dist;
@@ -20,7 +19,7 @@ public class ModKeybinds {
             new KeyMapping(
                     "key.era_tweaks.open_upgrade_screen",                     // translation key
                     InputConstants.Type.KEYSYM,                  // тип ввода
-                    GLFW.GLFW_KEY_G,                             // клавиша по умолчанию (G)
+                    GLFW.GLFW_KEY_G,                             // клавиша по умолчанию
                     "key.category.era_tweaks"                         // категория в Controls
             );
 
@@ -28,17 +27,40 @@ public class ModKeybinds {
             new KeyMapping(
                     "key.era_tweaks.open_class_screen",                     // translation key
                     InputConstants.Type.KEYSYM,                  // тип ввода
-                    GLFW.GLFW_KEY_H,                             // клавиша по умолчанию (G)
+                    GLFW.GLFW_KEY_H,                             // клавиша по умолчанию
                     "key.category.era_tweaks"                         // категория в Controls
             );
 
-    public static final KeyMapping OPEN_CLASS_SELECT_SCREEN =
+    public static final KeyMapping PASSIVE_SKILL_1 =
             new KeyMapping(
-                    "key.era_tweaks.open_class_select_screen",                     // translation key
+                    "key.era_tweaks.passive_skill_1",                     // translation key
                     InputConstants.Type.KEYSYM,                  // тип ввода
-                    GLFW.GLFW_KEY_F,                             // клавиша по умолчанию (G)
+                    GLFW.GLFW_KEY_KP_1,                             // клавиша по умолчанию
                     "key.category.era_tweaks"                         // категория в Controls
             );
+    public static final KeyMapping PASSIVE_SKILL_2 =
+            new KeyMapping(
+                    "key.era_tweaks.passive_skill_2",                     // translation key
+                    InputConstants.Type.KEYSYM,                  // тип ввода
+                    GLFW.GLFW_KEY_KP_2,                             // клавиша по умолчанию
+                    "key.category.era_tweaks"                         // категория в Controls
+            );
+    public static final KeyMapping PASSIVE_SKILL_3 =
+            new KeyMapping(
+                    "key.era_tweaks.passive_skill_3",                     // translation key
+                    InputConstants.Type.KEYSYM,                  // тип ввода
+                    GLFW.GLFW_KEY_KP_3,                             // клавиша по умолчанию
+                    "key.category.era_tweaks"                         // категория в Controls
+            );
+
+    public static final KeyMapping PASSIVE_SKILL_4 =
+            new KeyMapping(
+                    "key.era_tweaks.passive_skill_4",                     // translation key
+                    InputConstants.Type.KEYSYM,                  // тип ввода
+                    GLFW.GLFW_KEY_KP_4,                             // клавиша по умолчанию
+                    "key.category.era_tweaks"                         // категория в Controls
+            );
+
 
 
 
@@ -62,14 +84,31 @@ public class ModKeybinds {
                 }
             }
 
-            while (OPEN_CLASS_SELECT_SCREEN.consumeClick()) {
+            while (PASSIVE_SKILL_1.consumeClick()) {
                 Minecraft mc = Minecraft.getInstance();
                 if (mc.player != null) {
-                    var data = mc.player.getData(MyAttachments.PLAYER_CLASS);
-                    if (!data.getPlayerSubClass().equals("unknown")) {
-                        return;
-                    }
-                    mc.setScreen(new ClassSelectScreen());
+                    Network.toServerAction("active1", 10);
+                }
+            }
+
+            while (PASSIVE_SKILL_2.consumeClick()) {
+                Minecraft mc = Minecraft.getInstance();
+                if (mc.player != null) {
+                    Network.toServerAction("active2", 10);
+                }
+            }
+
+            while (PASSIVE_SKILL_3.consumeClick()) {
+                Minecraft mc = Minecraft.getInstance();
+                if (mc.player != null) {
+                    Network.toServerAction("active3", 10);
+                }
+            }
+
+            while (PASSIVE_SKILL_4.consumeClick()) {
+                Minecraft mc = Minecraft.getInstance();
+                if (mc.player != null) {
+                    Network.toServerAction("active4", 10);
                 }
             }
         }
@@ -81,7 +120,11 @@ public class ModKeybinds {
         public static void registerKeyMappings(RegisterKeyMappingsEvent event) {
             event.register(OPEN_UPGRADE_SCREEN);
             event.register(OPEN_CLASS_SCREEN);
-            event.register(OPEN_CLASS_SELECT_SCREEN);
+            event.register(PASSIVE_SKILL_1);
+            event.register(PASSIVE_SKILL_2);
+            event.register(PASSIVE_SKILL_3);
+            event.register(PASSIVE_SKILL_4);
+
         }
     }
 }
