c2e3b673f0fd4890d0e24a265c9ea9dcb4efe9d0
Artmaster629 <ponedilokartem2@gmail.com>
2026-02-02 17:21:10 +0200

    Показатель убер-заряда Скаута перенесён в отдельный класс

--- src/main/java/net/artmaster/era_tweaks/custom/gui/BossbarManager.java
+++ src/main/java/net/artmaster/era_tweaks/custom/gui/BossbarManager.java
@@ -1,4 +1,4 @@
-package net.artmaster.era_tweaks.client;
+package net.artmaster.era_tweaks.custom.gui;
 
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.network.chat.Component;
@@ -11,6 +11,33 @@ import java.util.Map;
 
 public class BossbarManager {
     private static final Map<ServerPlayer, ServerBossEvent> bossbars = new HashMap<>();
+
+    public static void updateBossbarUber(ServerPlayer player, double progress, double progress_addition) {
+
+        Component titleAttribute = Component.translatable("text.era_tweaks.uber");
+
+        ServerBossEvent bossbar = bossbars.computeIfAbsent(player, p -> {
+            ServerBossEvent newBar = new ServerBossEvent(
+                    titleAttribute,
+                    BossBarColor.YELLOW,
+                    BossBarOverlay.PROGRESS
+            );
+            newBar.setCreateWorldFog(false);
+            newBar.setVisible(true);
+            newBar.addPlayer(player);
+            return newBar;
+        });
+
+
+        double visualProgress = progress;
+        if (progress > 100) {
+            visualProgress=100;
+        }
+        bossbar.setProgress((float) (visualProgress/100)); // от 0.0 до 1.0
+
+
+        bossbar.setName(Component.literal(titleAttribute.getString()+" +"+(double) Math.round(progress_addition*100)/100+" ("+ (int) visualProgress + "%)"));
+    }
 
     public static void updateBossbar(ServerPlayer player, double progress, double progress_addition, String attribute) {
 
