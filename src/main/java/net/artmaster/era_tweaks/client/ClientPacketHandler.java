d191dcf8fd87164f7daf27ea795cdc20e42e8035
Artmaster629 <ponedilokartem2@gmail.com>
2026-02-02 17:29:53 +0200

    Добавлена синхронизация на клиенте новых хранилищ

--- src/main/java/net/artmaster/era_tweaks/client/ClientPacketHandler.java
+++ src/main/java/net/artmaster/era_tweaks/client/ClientPacketHandler.java
@@ -1,21 +1,33 @@
 package net.artmaster.era_tweaks.client;
 
 import com.alrex.parcool.api.Stamina;
-import net.artmaster.era_tweaks.api.container.MyAttachments;
-import net.artmaster.era_tweaks.api.container.PlayerClassData;
-import net.artmaster.era_tweaks.api.container.PlayerSAttrubitesData;
+import net.artmaster.era_tweaks.network.main_packets.ClientActionPacket;
+import net.artmaster.era_tweaks.network.main_packets.ServerActionPacket;
+import net.artmaster.era_tweaks.network.main_packets.DataPacket;
+import net.artmaster.era_tweaks.registry.ModAttachments;
+import net.artmaster.era_tweaks.custom.data.OverlayAttributesData;
+import net.artmaster.era_tweaks.custom.data.PlayerClassData;
+import net.artmaster.era_tweaks.custom.data.PlayerSAttrubitesData;
+import net.artmaster.era_tweaks.custom.player_classes.warrior.scout.custom.ScoutUberData;
 import net.artmaster.era_tweaks.client.screen.ClassManageScreen;
 import net.artmaster.era_tweaks.client.screen.ClassSelectScreen;
 import net.artmaster.era_tweaks.client.screen.UpgradeManageScreen;
 import net.artmaster.era_tweaks.network.*;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.api.distmarker.OnlyIn;
 
 
 @OnlyIn(Dist.CLIENT)
 public class ClientPacketHandler {
+
+
+
     public static void handleOpenGui(OpenGuiPacket packet) {
         if (packet.resourceId().equals("upgrade_manage_screen")) {
             Minecraft.getInstance().setScreen(new UpgradeManageScreen());
@@ -28,21 +40,36 @@ public class ClientPacketHandler {
         }
     }
 
-
-
-    public static void handleSyncPlayerSkills(SyncPlayerSkillsPacket packet) {
+    public static void handleSyncPlayerClass(DataPacket packet) {
         var player = Minecraft.getInstance().player;
         if (player == null) return;
 
-        var data = player.getData(MyAttachments.PLAYER_SKILLS);
-        PlayerSAttrubitesData.load(data, packet.data());
-    }
+        switch (packet.dataType()) {
+            case 1 -> {
+                var data = player.getData(ModAttachments.PLAYER_SKILLS);
+                PlayerSAttrubitesData.load(data, packet.data());
+                Screen screen = Minecraft.getInstance().screen;
+                if (screen instanceof UpgradeManageScreen upgradeScreen) {
+                    upgradeScreen.refresh();
+                }
+            }
+            case 2 -> {
+                var data = player.getData(ModAttachments.PLAYER_CLASS);
+                PlayerClassData.load(data, packet.data());
+                Screen screen = Minecraft.getInstance().screen;
+                if (screen instanceof UpgradeManageScreen upgradeScreen) {
+                    upgradeScreen.refresh();
+                }
+            }
+            case 3 -> {var data = player.getData(ModAttachments.OVERLAYS_DATA); OverlayAttributesData.load(data, packet.data());}
+            case 4 -> {var data = player.getData(ModAttachments.UBER); ScoutUberData.load(data, packet.data());}
+        }
 
-    public static void handleSyncPlayerClass(SyncPlayerClassPacket packet) {
-        var player = Minecraft.getInstance().player;
-        if (player == null) return;
-        var data = player.getData(MyAttachments.PLAYER_CLASS);
-        PlayerClassData.load(data, packet.data());
+        // 1 - атрибуты
+        // 2 - класс
+        // 3 - оверлей
+        // 4 - убер
+
     }
 
     public static void handleStaminaConsume(StaminaPacket packet) {
@@ -59,6 +86,18 @@ public class ClientPacketHandler {
         LocalPlayer player = Minecraft.getInstance().player;
         if (player != null) {
             player.connection.sendCommand(packet.command());
+        }
+    }
+
+    public static void handleClientAction(ClientActionPacket packet) {
+        if (packet.actionType() == 1) {
+            ItemStack item = new ItemStack(Items.TOTEM_OF_UNDYING);
+            Minecraft.getInstance().gameRenderer.displayItemActivation(item);
+        }
+
+        if (packet.actionType() == 2) {
+            LocalPlayer player = Minecraft.getInstance().player;
+            player.displayClientMessage(Component.literal(packet.key()), false);
         }
     }
 }
